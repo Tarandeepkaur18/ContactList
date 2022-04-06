a6e18817bb7d369bd05ad3d17cdefc86a629ad85
Tarandeepkaur18 <102780701+Tarandeepkaur18@users.noreply.github.com>
2022-04-06 09:34:14 +0530

    commit and push

--- app/src/main/java/com/example/contactactivity/FirestoreActivity.kt
+++ app/src/main/java/com/example/contactactivity/FirestoreActivity.kt
@@ -5,13 +5,16 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.contactactivity.databinding.ActivityFirestoreBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FirestoreActivity : AppCompatActivity() {
+    lateinit var binding : ActivityFirestoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_firestore)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_firestore)
         val db = Firebase.firestore
         val user = hashMapOf(
             "first" to "Taran",
@@ -20,24 +23,27 @@ class FirestoreActivity : AppCompatActivity() {
             "Rollno" to "1803600"
 
         )
-
-        db.collection("documents").document("details of student")
-            .set(user)
-            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+binding.btnadd.setOnClickListener {
+    db.collection("documents").document("details of student")
+        .set(user)
+        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+        .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+}
+        binding.btnupdate.setOnClickListener {
 
         db.collection("documents").document("details of student")
             .update(mapOf(
                 "first" to "tarandeep",
                 "last" to "kaur"
-            ))
-       // db.collection("documents").document("details of student").delete().addOnSuccessListener{
-           // Log.d(TAG, "DocumentSnapshot successfully deleted!") }
-            //.addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
-        // Deleting collections from an Android client is not recommended.
+            ))}
+        binding.btndelete.setOnClickListener {
+             db.collection("documents").document("details of student").delete().addOnSuccessListener{
+             Log.d(TAG, "DocumentSnapshot successfully deleted!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error deleting document", e) }
+            // Deleting collections from an Android client is not recommended.
 
 
-
+        }
 
     }
 }
